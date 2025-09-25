d340f0f42cc521b782cb7295781137a1d15f8685
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2025-09-25 13:57:42 +0200

    yep

--- tests/test_implicit.c
+++ tests/test_implicit.c
@@ -2,7 +2,7 @@
 #include "cme/c_assets_implicit.h"
 
 int main(void) {
-    printf("size: %llu\n", subfolder_stuff_txt_size);
-    printf("size: %llu\n", subfolder_morestuff_txt_size);
+    printf("size: %lu\n", subfolder_stuff_txt_size);
+    printf("size: %lu\n", subfolder_morestuff_txt_size);
     return 0;
 }
