133cde1f74d63bb5f98a8ee99eeab8011c030cc5
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2025-09-25 14:02:39 +0200

    improve cxx standard enforcement and error handling (#1)
    * improve cxx standard enforcement and error handling
    
    * add fixes for clang on windows

--- tests/test_explicit.c
+++ tests/test_explicit.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "cme/c_assets_explicit.h"
 
-int main() {
+int main(void) {
     printf("size: %lu\n", subfolder_stuff_txt_size);
     printf("size: %lu\n", subfolder_morestuff_txt_size);
     return 0;
